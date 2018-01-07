e9d70bb1b6fc190ca5c0adc3df5608ca52d40c9a
Bharsiya <dbharsiya@deloitte.com>
2018-01-07 13:39:35 +0530

    customized modal

--- src/app/common-services/modal-binder.service.ts
+++ src/app/common-services/modal-binder.service.ts
@@ -1,17 +1,37 @@
 import { ComponentFactoryResolver, Injectable } from "@angular/core";
 import { CustomerDetailComponent } from "../customer-detail/customer-detail.component";
-
+import { ModalCommonComponent } from "../common/modal-common/modal-common.component";
 @Injectable()
 export class ModalBinderService {
-    rootViewContainer :any;
+    private rootViewContainer: any;
     constructor(private factoryResolver: ComponentFactoryResolver) { }
+    public currentModalCompName: string = '';
+    private modalComponents: any = [
+        { 'teamModal': CustomerDetailComponent }
+    ];
+    dynamicComponent: any;
     setRootViewContainerRef(viewContainerRef) {
         this.rootViewContainer = viewContainerRef
     }
 
     addDynamicComponent() {
-        const factory = this.factoryResolver.resolveComponentFactory(CustomerDetailComponent);
+        const factory = this.factoryResolver.resolveComponentFactory(this.getModalComponent());
         const component = factory.create(this.rootViewContainer.parentInjector);
         this.rootViewContainer.insert(component.hostView);
+    }
+
+    setModalName(modalName: string) {
+        this.currentModalCompName = modalName;
+    }
+
+    getModalComponent(): any {
+
+        for (let element of this.modalComponents) {
+            if (element[this.currentModalCompName]) {
+                this.dynamicComponent = element[this.currentModalCompName];
+                break;
+            }
+        }
+        return this.dynamicComponent;
     }
 }
--- src/app/common/modal-common/modal-opener.service.ts
+++ src/app/common/modal-common/modal-opener.service.ts
@@ -5,8 +5,9 @@ import { ModalBinderService } from "../../common-services/modal-binder.service";
 
 @Injectable()
 export class ModalOpenerService {
-    constructor(private modalService: NgbModal) {}
+    constructor(private modalService: NgbModal, private modalBinder: ModalBinderService) {}
     openModal(modalName:string): void {
+        this.modalBinder.setModalName(modalName);
         const modalRef = this.modalService.open(ModalCommonComponent);
       //  modalRef.componentInstance.name = 'World';
     }
